299d712f49ddae680072d2f7a15aa76305b9331f
dmitrii.voronin <VoronindimTvink123@mail.ru>
2021-11-28 10:53:57 +0300

    Initial Commit

---
+++ FeatureLogin/FeatureLogin.h
@@ -0,0 +1,18 @@
+//
+//  FeatureLogin.h
+//  FeatureLogin
+//
+//  Created by Dmitrii Voronin on 28.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeatureLogin.
+FOUNDATION_EXPORT double FeatureLoginVersionNumber;
+
+//! Project version string for FeatureLogin.
+FOUNDATION_EXPORT const unsigned char FeatureLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureLogin/PublicHeader.h>
+
+
